85be64358a5de1dbf1aaa99a5e59626845a3e937
ashishpateldev <iashishpatel11@gmail.com>
2023-12-25 23:04:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,48 @@ function App() {
       return (
             <>
                   <div class='flex'>
-                        <div class='h-[100vh] w-[28em] bg-white m-0 p-0'>
-                              <h1>Pocket Notes</h1>
+                        <div class='h-[100vh] w-[24em] bg-white m-0 p-0'>
+                              <h1 class='text-4xl font-bold ml-20 mt-8 mb-8'>Pocket Notes</h1>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(0,71,255,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>MN</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>My Notes</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(179,139,250,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>MP</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>My Personal grp</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(255,192,192,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>JG</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>Javascript grp</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(67,230,252,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>HG</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>HTML grp</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(241,149,118,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>CN</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>CSS Notes</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(102,145,255,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>SN</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>SQL Notes</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(255,102,240,1)] rounded-full text-white flex items-center justify-center ml-4 font-bold mb-5'>PN</div>
+                                    <p class='text-xl font-bold m-4 ml-7'>Python Notes</p>
+                              </div>
+
+                              <div class='flex'>
+                                    <div class='h-16 w-16 bg-[rgba(22,0,139,1)] rounded-full text-white flex items-center justify-center ml-[8em] font-bold mb-5 text-4xl'>+</div>
+                              </div>
+
                         </div>
 
 
